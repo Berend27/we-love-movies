7fafcaffe8bc366d6723d7aed4cf811d60aceacd
Dexter Berend <dexterjberend@gmail.com>
2021-11-06 23:29:27 -0500

    GET /movies?is_showing=true

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -2,7 +2,9 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundry");
 const moviesService = require("./movies.service");
 
 async function list(req, res, next) {
-    const data = await moviesService.list();
+    const data = req.query.is_showing ? 
+        await moviesService.listIfIsShowing() :
+        await moviesService.list();
     res.json({ data });
 }
 
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -6,6 +6,14 @@ function list() {
     return knex("movies").select("*");
 }
 
+function listIfIsShowing() {
+    return knex("movies as m")
+        .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+        .distinct("m.*")
+        .where("is_showing", true);
+}
+
 module.exports = {
     list,
+    listIfIsShowing,
 }

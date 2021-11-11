083de2a04ea64d2a739fefff331f60f7476b9e04
Dexter Berend <dexterjberend@gmail.com>
2021-11-10 22:55:15 -0600

    edited Procfile to start with the server file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 if (process.env.USER) require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
-
-const cors = require("cors");
-
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
